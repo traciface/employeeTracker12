a1add784e5a20cf50b6f3377703b7c4eded47b5d
Traci <traci.face@gmail.com>
2021-05-12 22:31:31 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'root',
+  database: 'employee_db',
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    runSearch();
+  });
+  
+//   connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+//     if (error) throw error;
+//     console.log('The solution is: ', results[0].solution);
+//   });
+
+
+const runSearch = () => {
+    inquirer
+      .prompt({
+        name: 'action',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+          'View all employees',
+          'View all roles',
+          'View all departments',
+          'Add Employee',
+          'Add Role',
+          'Add Department',
+          'Update Employee Role',
+          'exit',
+        ],
+      })
+  };
+  
+
+  connection.end();
