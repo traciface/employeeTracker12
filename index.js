6e3068738a2a0a0d81cd31f359e65cf2ba66502c
Traci <traci.face@gmail.com>
2021-05-17 19:55:42 -0400

    added change employee role

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const runSearch = () => {
             viewDepartments();
             break;
           case 'Add Employee':
-            addEmployee();
+            getManagers();
             break;
           case 'Add Role':
             addRole();
@@ -59,7 +59,7 @@ const runSearch = () => {
             addDepartment();
             break;
           case 'Update Employee Role':
-            updateEmployeeRole();
+            getEmployees();
             break;
           case "Exit":
               connection.end();
@@ -125,6 +125,14 @@ const addDepartment = () =>{
 };
 
 const addRole = () =>{
+  connection.query("SELECT * FROM department", (err, res) => 
+  {
+    if (err) throw err;
+   departmentChoices = res.map(({ id, name }) => ({
+      name: name,
+      value: id
+    }))
+  
   inquirer
   .prompt([
     {
@@ -139,8 +147,9 @@ const addRole = () =>{
   },
   {
     name: 'departmentID',
-    type: 'input',
-    message: 'What is the department ID'
+    type: 'list',
+    message: 'What is the department ID',
+    choices: departmentChoices
   }
   ])
   
@@ -153,17 +162,39 @@ const addRole = () =>{
     })
     runSearch();
   })
+})
 };
 
-const addEmployee = () =>{
-  var roles = [];
+const getManagers = () =>
+{
+  connection.query("SELECT * FROM employee", (err, res) => 
+  {
+    if (err) throw err;
+   managerChoices = res.map(({ id, first_name, last_name }) => ({
+      name: first_name + last_name,
+      value: id
+    }))
+    getRoles(managerChoices)
+  })
+  };
+
+  const getRoles = (managerChoices) =>
+{
+  connection.query("SELECT * FROM role", (err, res) => 
+  {
+    if (err) throw err;
+   roleChoices = res.map(({ id, title }) => ({
+      name: title,
+      value: id
+    }))
+    addEmployee(managerChoices, roleChoices);
+  })
   
-  // connection.query("SELECT * FROM role", () => {
-  //  var roleChoices = roles.map(({ id, title }) => ({
-  //     name: title,
-  //     value: id
-  //   }))
-  // })
+  };
+
+const addEmployee = (managerChoices, roleChoices) =>
+{
+  
   inquirer
   .prompt([
     {
@@ -178,14 +209,15 @@ const addEmployee = () =>{
   },
   {
     name: 'roleID',
-    type: 'input',
+    type: 'list',
     message: 'What is the role ID',
-    // choices: roleChoices
+    choices: roleChoices
   },
   {
      name: 'managerID',
-    type: 'input',
-    message: 'What is the manager ID'
+    type: 'list',
+    message: 'What is the manager ID',
+    choices: managerChoices
   }
   ])
   
@@ -202,21 +234,60 @@ const addEmployee = () =>{
   })
 };
 
-// const updateEmployeeRole = () =>{
-//   inquirer
-//   .prompt({
-//     name: 'action',
-//     type: 'list',
-//     message: 'What would you like to do?',
-//     choices: [
-//       'View all Employees',
-//       'View all Roles',
-//       'View all Departments',
-//       'Add Employee',
-//       'Add Role',
-//       'Add Department',
-//       'Update Employee Role',
-//       'Exit',
-//     ]
-//   })
-// }
+
+const getEmployees = () =>
+{
+  connection.query("SELECT * FROM employee", (err, res) => 
+  {
+    if (err) throw err;
+    employeechoices = res.map(({ id, first_name, last_name }) => ({
+      name: first_name + last_name,
+      value: id
+    }))
+    getEmployeeRoles(employeechoices)
+  })
+  };
+
+  const getEmployeeRoles = (employeechoices) =>
+{
+  connection.query("SELECT * FROM role", (err, res) => 
+  {
+    if (err) throw err;
+   roleChoices = res.map(({ id, title }) => ({
+      name: title,
+      value: id
+    }))
+    updateEmployeeRole(employeechoices, roleChoices);
+  })
+  
+  };
+
+const updateEmployeeRole = (employeechoices, roleChoices) =>
+{
+  
+  inquirer
+  .prompt([
+    {
+    name: 'employeeID',
+    type: 'list',
+    message: 'Who is the employee that will be changed?',
+    choices: employeechoices
+  },
+  {
+    name: 'newRole',
+    type: 'list',
+    message: 'Select new role',
+    choices: roleChoices
+  }
+  ])
+  
+  .then((answer) => {
+    const query = `update employee SET role_ID = ? WHERE id = ?`
+    connection.query(query, [
+       answer.newRole,
+       answer.employeeID
+    ])
+    runSearch();
+  })
+};
+
